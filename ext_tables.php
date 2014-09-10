7522c96e3272380f09080b0d40699aa7c6ab6b07
Cretection <info@cretection.eu>
2014-09-10 14:56:00 +0200

    Initial Release
    Erster Release mit folgenden Funktionen:
    
    TYPO3 Backend Login Anpassung
    TYPO3 Pages erweitert um Linktitle

---
+++ ext_tables.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+$tempColumns = array(
+	'tx_crtstdfunc_linktitle' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:crt_std_func/Resources/Private/Language/locallang_db.xlf:pages.tx_crtstdfunc_linktitle.label',
+		'config' => array(
+			'type' => 'input',
+			'eval' => 'required',
+		),
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	$_EXTKEY,
+	'Configuration/TypoScript',
+	'Cretection Standard Functions'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+	'pages',
+	$tempColumns,
+	1
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+	'pages_language_overlay',
+	$tempColumns,
+	1
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+	'pages',
+	'title',
+	'tx_crtstdfunc_linktitle',
+	'after:nav_title'
+);
+
+$TBE_STYLES['logo_login'] = '../typo3conf/ext/crt_std_func/Resources/Public/Images/t3skin/logo.png';
+$TBE_STYLES['inDocStyles_TBEstyle'] .='
+body#typo3-index-php {
+	background: none!importent;
+	background: #27AE60;
+}
+body#typo3-index-php .t3-login-logo {
+	margin: 15px 0;
+}
+body#typo3-index-php .t3-headline {
+	background: #334455;
+}
+body#typo3-index-php input.t3-login-submit {
+	border-top: 1px solid #D55300;
+	border-left: 1px solid #D55300;
+	background: #D55300!important;
+}
+body#typo3-index-php div#t3-copyright-notice {
+	color: #334455;
+}
+';
---
+++ ext_tables.sql
@@ -0,0 +1,13 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+    tx_crtstdfunc_linktitle varchar(255) DEFAULT '' NOT NULL
+);
+
+#
+# Table structure for table 'pages_language_overlay'
+#
+CREATE TABLE pages_language_overlay (
+    tx_crtstdfunc_linktitle varchar(255) DEFAULT '' NOT NULL
+);
