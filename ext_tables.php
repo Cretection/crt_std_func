39d036e9a5a1d1b1cd9cc4abc8c33e170fbe138b
Cretection <info@cretection.eu>
2014-09-13 13:06:36 +0200

    Changes
    Änderungen am Code wie Backend Icons eingebunden werden. Alte Version
    gelöscht und 2 Icons hinzugefügt

--- ext_tables.php
+++ ext_tables.php
@@ -35,33 +35,32 @@ $tempColumns = array(
 	'after:nav_title'
 );
 
-//\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
-
-// New icons for the BE
-//if (TYPO3_MODE == 'BE') {
-//    $icons = array('anchor', 'attach', 'bookaddresses', 'brick', 'bricks', 'calendar', 'chartBar', 'chartorganisation', 'cog', 'comment', 'email', 'feed',	'layers', 'map', 'newspaper', 'note', 'plugin',	'report', 'scriptcode', 'scriptgear', 'server');
-//    foreach ($icons as $icon) {
-//        \TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
-//            'pages',
-//            'contains-' . $icon,
-//            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Backend/Icons/' . $icon . '.png');
-//        $TCA['pages']['columns']['module']['config']['items'][] = array(
-//            ucfirst($icon),
-//            $icon,
-//            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Backend/Icons/' . $icon . '.png'
-//        );
-//    }
-//}
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
+
 if (TYPO3_MODE == 'BE') {
 	\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
 		'pages',
 		'contains-bricks',
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bricks.png'
 	);
-	$TCA['pages']['columns']['module']['config']['items'][] = array('Bricks',bricks,\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bricks.png');
+	$TCA['pages']['columns']['module']['config']['items'][] = array(
+		'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.bricks',
+		bricks,
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bricks.png'
+	);
+	\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
+		'pages',
+		'contains-bookaddresses',
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bookaddresses.png'
+	);
+	$TCA['pages']['columns']['module']['config']['items'][] = array(
+		'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.bookaddresses',
+		bookaddresses,
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bookaddresses.png'
+	);
 }
+
 
 // Change Backend Login Layout
 $TBE_STYLES['logo_login'] = '../typo3conf/ext/crt_std_func/Resources/Public/Backend/Images/logo.png';
