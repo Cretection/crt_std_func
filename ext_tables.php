6f6259cb89c3b09186aaca9fd581c2e2211b9180
Cretection <info@cretection.eu>
2014-09-13 13:50:09 +0200

    Added all Backend Icons
    Backend Icons hinzugefügt und Script nochmals optimiert

--- ext_tables.php
+++ ext_tables.php
@@ -39,28 +39,21 @@ $tempColumns = array(
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
 
 if (TYPO3_MODE == 'BE') {
-	\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
-		'pages',
-		'contains-bricks',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bricks.png'
-	);
-	$TCA['pages']['columns']['module']['config']['items'][] = array(
-		'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.bricks',
-		bricks,
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bricks.png'
-	);
-	\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
-		'pages',
-		'contains-bookaddresses',
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bookaddresses.png'
-	);
-	$TCA['pages']['columns']['module']['config']['items'][] = array(
-		'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.bookaddresses',
-		bookaddresses,
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/bookaddresses.png'
-	);
+	// Iconname darf nicht länger als 10 Zeichen sein!
+	$icons = array('adress', 'anchor', 'attach', 'brick', 'bricks', 'calendar', 'chartbar', 'cog', 'comment', 'email', 'feed', 'layers', 'map', 'newspaper', 'note', 'orga', 'plugin', 'report', 'scriptcode', 'scriptgear', 'server');
+	foreach ($icons as $icon) {
+		\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
+			'pages',
+			'contains-'.$icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/'.$icon.'.png'
+		);
+		$TCA['pages']['columns']['module']['config']['items'][] = array(
+			'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.'.$icon,
+			$icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Backend/Icons/'.$icon.'.png'
+		);
+	}
 }
-
 
 // Change Backend Login Layout
 $TBE_STYLES['logo_login'] = '../typo3conf/ext/crt_std_func/Resources/Public/Backend/Images/logo.png';
