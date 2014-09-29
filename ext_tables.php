a244a269cb5b9fc32044e40d5c2eecaa82cea6f2
Cretection <info@cretection.eu>
2014-09-29 03:00:12 +0200

    Fix Bug
    Bug ist gefixt

--- ext_tables.php
+++ ext_tables.php
@@ -50,13 +50,30 @@ $pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
  * @todo adjust compatibility TYPO3 6.1
  */
  
-$recordTypes = array('Pages','SysNews','BeUsers');
+$recordTypes = array('SysNews','BeUsers');
 $configurationPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'/Configuration/TCA';
 foreach ($recordTypes as $recordType) {
     $configurationFile = $configurationPath.'/'.$recordType.'.php';
     if (file_exists($configurationFile)) {
         include_once ($configurationFile);
     }
+}
+\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
+if (TYPO3_MODE == 'BE') {
+	// Iconname darf nicht länger als 10 Zeichen sein!
+	$icons = array('address', 'anchor', 'attach', 'brick', 'bricks', 'calendar', 'chartbar', 'cog', 'comment', 'email', 'feed', 'layers', 'map', 'newspaper', 'note', 'orga', 'plugin', 'report', 'scriptcode', 'scriptgear', 'server');
+	foreach ($icons as $icon) {
+		\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
+			'pages',
+			'contains-'.$icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($$_EXTKEY).'Resources/Public/Icons/Backend/'.$icon.'.png'
+		);
+		$TCA['pages']['columns']['module']['config']['items'][] = array(
+			'LLL:EXT:'.$extKey.'/Resources/Private/Language/locallang_db.xlf:tca.pages.module.items.'.$icon,
+			$icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'Resources/Public/Icons/Backend/'.$icon.'.png'
+		);
+	}
 }
 
 // Change Backend Login Layout
