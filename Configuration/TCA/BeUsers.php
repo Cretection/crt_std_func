4d44ccbe556e648635e7cfdf405d287b0e9962e1
Cretection <info@cretection.eu>
2015-04-17 18:10:57 +0200

    Delete loadTCA()
    Is deprecated

--- Configuration/TCA/BeUsers.php
+++ Configuration/TCA/BeUsers.php
@@ -8,7 +8,7 @@
 
 # Allows BE User to be put on different page as pid = 0
 
-\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('be_users');
+//\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('be_users');
 $TCA['be_users']['ctrl']['rootLevel'] = -1;
 
 ?>
